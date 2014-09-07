f60884d05b3cf1cad730bf796f24faa15517ae50
Sergey Maskalik <mercury2269@gmail.com>
2014-09-07 16:30:59 -0700

    Finished

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import "net/http"
+import "encoding/json"
+import "strings"
 
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+		data, err := query(city)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+	})
 	http.ListenAndServe(":8081", nil)
 }
 
@@ -12,8 +26,25 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 type weatherData struct {
-	Name string 'json:"name"'
+	Name string `json:"name"`
 	Main struct {
-		Kelvin float64 'json:"temp"'
-	} 'json:"main"'
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func query(city string) (weatherData, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+
+	return d, nil
 }
